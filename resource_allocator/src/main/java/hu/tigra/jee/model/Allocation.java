9049bdc2fda0dcc0b066ccc6d4405aff8697f489
akmedone <syadam@outlook.hu>
2016-10-12 15:51:30 +0200

    kész

--- resource_allocator/src/main/java/hu/tigra/jee/model/Allocation.java
+++ resource_allocator/src/main/java/hu/tigra/jee/model/Allocation.java
@@ -35,13 +35,13 @@ public class Allocation extends EqualsById implements Serializable {
     @NotNull
     @NotEmpty
     @Pattern(regexp = "^((19|20)\\d\\d)-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01]) ([01]?[0-9]|2[0-3]):[0-5][0-9]$",
-            message = "Example: 2016-07-13 19:45")
+            message = "Example: 2016-10-12 12:00")
     private String start;
 
     @NotNull
     @NotEmpty
     @Pattern(regexp = "^((19|20)\\d\\d)-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01]) ([01]?[0-9]|2[0-3]):[0-5][0-9]$",
-            message = "Example: 2016-07-13 19:45")
+            message = "Example: 2016-10-12 12:00")
     private String stop;
 
     @Override
